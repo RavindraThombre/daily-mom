ce89d0906750d166eca05f0eb5842be3502ea8aa
RavindraThombre <ravindrathombre66@gmail.com>
2026-05-04 09:34:46 +0530

    fix: build bug

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/mom",
+
   allowedDevOrigins: ["http://localhost:3000", "http://192.168.1.109:3000"],
+
   turbopack: {
     root: __dirname,
   },
+
   async redirects() {
-    if (process.env.NODE_ENV === "development") {
-      return [
-        {
-          source: "/",
-          destination: "/mom",
-          permanent: false,
-          basePath: false,
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: "/",
+        destination: "/mom",
+        permanent: false,
+        basePath: false,
+      },
+    ];
   },
 };
 
